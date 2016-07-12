053f46d2e176820e774c0f077dfa29b9c2d18da0
Grant Miiller <grant.miiller@nytimes.com>
2016-07-12 16:29:18 -0400

    Bash complete

---
+++ bash_completion.d
@@ -0,0 +1,23 @@
+# The function that is called to autocomplete
+_notenames()
+{
+    local cur prev opts
+    # The special variable to show what the tab should reply as
+    COMPREPLY=()
+
+    # The current word, for instance:
+    # `command prev cur`
+    cur="${COMP_WORDS[COMP_CWORD]}"
+
+    # The previous word, for instance:
+    # `command prev cur`
+    prev="${COMP_WORDS[COMP_CWORD-1]}"
+
+    # The options to compare the word against to autocomplete
+    opts=$(./m2notes --getlist)
+
+    COMPREPLY=( $(compgen -W "${opts}" -- ${cur}) )
+    return 0
+}
+
+complete -F _notenames m2notes
